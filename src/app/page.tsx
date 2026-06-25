c6c9ee527cfd11b9b788f89fb570f551408f07c3
Atanazio01 <marcosd.atanazio@gmail.com>
2026-06-25 16:16:29 -0300

    When to use a Server Component or a Client Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,16 @@
 //Server Component
 
-async function getUsers() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  return response.json();
-}
-
 export default async function Home() {
-  const users = await getUsers();
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+
+  console.log("Oi, belezinha?")
+
   return (
     <div>
-      Hello JStack
-      <ul>
-        {users.map((user: { id: number; name: string }) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <button onClick={() => alert('Ok')}>
+        Clicou
+      </button>
     </div>
   );
 }
